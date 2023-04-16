c28f1e412f49812d97a7a09f97a931c95b7c8180
Ryoh Akiyoshi <ryo614@gmail.com>
2023-04-16 17:41:25 +0900

    solved joi2008yo/a

---
+++ chapter01/joi2008yo_a/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func main() {
+	n := nextInt()
+	n = 1000 - n
+	var count int
+	count += n / 500
+	n -= (n / 500) * 500
+	count += n / 100
+	n -= (n / 100) * 100
+	count += n / 50
+	n -= (n / 50) * 50
+	count += n / 10
+	n -= (n / 10) * 10
+	count += n / 5
+	n -= (n / 5) * 5
+	count += n
+	fmt.Println(count)
+}
+
+func init() {
+	if len(os.Args) > 1 && os.Args[1] == "debug" {
+		if len(os.Args) == 2 {
+			fmt.Fprintf(os.Stderr, "filename is required")
+			os.Exit(1)
+		}
+		debug(os.Args[2])
+	}
+
+	sc.Split(bufio.ScanWords)
+
+	buf := make([]byte, 10*1024)
+	sc.Buffer(buf, math.MaxInt32)
+}
+
+func debug(filename string) {
+	testFile, err := os.Open(filename)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s: no such file", filename)
+		os.Exit(1)
+	}
+	sc = bufio.NewScanner(testFile)
+}
+
+func next() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	n, err := strconv.Atoi(next())
+	if err != nil {
+		fmt.Printf("%v", err)
+		os.Exit(1)
+	}
+	return n
+}
+
+func nextInts(n int) []int {
+	a := make([]int, n)
+	for i := 0; i < n; i++ {
+		a[i] = nextInt()
+	}
+	return a
+}
