fc3bfa89cd561a6b5f9e446b99a9bee7d6c738fa
Ryoh Akiyoshi <ryo614@gmail.com>
2023-04-18 21:56:49 +0900

    solved abc007/b

---
+++ chapter01/abc007_2/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func main() {
+	s := next()
+	if s == "a" {
+		fmt.Println(-1)
+	} else {
+		fmt.Println("a")
+	}
+}
+
+func init() {
+	if len(os.Args) > 1 && os.Args[1] == "debug" {
+		if len(os.Args) == 2 {
+			fmt.Fprintf(os.Stderr, "filename is required")
+			os.Exit(1)
+		}
+		debug(os.Args[2])
+	}
+
+	sc.Split(bufio.ScanWords)
+
+	buf := make([]byte, 10*1024)
+	sc.Buffer(buf, math.MaxInt32)
+}
+
+func debug(filename string) {
+	testFile, err := os.Open(filename)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s: no such file", filename)
+		os.Exit(1)
+	}
+	sc = bufio.NewScanner(testFile)
+}
+
+func next() string {
+	sc.Scan()
+	return sc.Text()
+}
