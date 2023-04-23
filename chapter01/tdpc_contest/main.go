33ed5ad0c60b9a844c415494928ff2ed24343b26
Ryoh Akiyoshi <ryo614@gmail.com>
2023-04-23 13:19:14 +0900

    solved tdpc/a

---
+++ chapter01/tdpc_contest/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func main() {
+	n := nextInt()
+	p := nextInts(n)
+	dp := make([][]int, n+1)
+	dp[0] = []int{0}
+	for i := 0; i < n; i++ {
+		next := make(map[int]struct{})
+		for _, v := range dp[i] {
+			next[v] = struct{}{}
+			next[v+p[i]] = struct{}{}
+		}
+		for v := range next {
+			dp[i+1] = append(dp[i+1], v)
+		}
+	}
+	fmt.Println(len(dp[n]))
+}
+
+func init() {
+	if len(os.Args) > 1 && os.Args[1] == "debug" {
+		if len(os.Args) == 2 {
+			fmt.Fprintf(os.Stderr, "filename is required")
+			os.Exit(1)
+		}
+		debug(os.Args[2])
+	}
+
+	sc.Split(bufio.ScanWords)
+
+	buf := make([]byte, 10*1024)
+	sc.Buffer(buf, math.MaxInt32)
+}
+
+func debug(filename string) {
+	testFile, err := os.Open(filename)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s: no such file", filename)
+		os.Exit(1)
+	}
+	sc = bufio.NewScanner(testFile)
+}
+
+func next() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	n, err := strconv.Atoi(next())
+	if err != nil {
+		fmt.Printf("%v", err)
+		os.Exit(1)
+	}
+	return n
+}
+
+func nextInts(n int) []int {
+	a := make([]int, n)
+	for i := 0; i < n; i++ {
+		a[i] = nextInt()
+	}
+	return a
+}
